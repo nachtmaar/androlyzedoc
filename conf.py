6387de8623baece8b58f15fedca553665f5420f9
Nils Schmidt <schmidt89@mathematik.uni-marburg.de>
2015-06-25 13:21:42 +0200

    correct path in conf.py for autoclass directive

--- conf.py
+++ conf.py
@@ -14,14 +14,14 @@
 import sys
 import os
 
-# If extensions (or modules to document with autodoc) are in another directory,
 # on_rtd is whether we are on readthedocs.org
-import os
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
+# If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('androlyze/'))
+sys.path.insert(0, os.path.abspath('./'))
+sys.path.insert(1, os.path.abspath('androlyze/'))
 sys.path.append(os.path.abspath("androguard"))
 
 # twitter bootstrap
